182b2bc75cc5a2ca76a6f9a59e2e1505fffa318d
Mirko-r <rovere04@gmail.com>
2024-01-29 13:58:48 +0100

    Updated db file

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,16 +1,21 @@
 -- Creating the Machines table
 CREATE TABLE Machines (
-    machineid INT PRIMARY KEY,
+    machineid varchar(50) PRIMARY KEY,
     location VARCHAR(255) NOT NULL
 );
 
 -- Creating the Productions table
 CREATE TABLE Productions (
     productionid INT PRIMARY KEY,
-    machineid INT FOREIGN KEY REFERENCES Machines(machineid),
+    machineid varchar(50) FOREIGN KEY REFERENCES Machines(machineid),
     year INT,
     month INT,
     day INT,
     hourofday INT CHECK (hourofday >= 0 AND hourofday <= 24),
     make int -- Adjust the data type based on the actual nature of 'make'
 );
+
+
+insert into Machines (machineid, location) 
+values('LSD23431OP', 'Milan'),
+('LLOPRER13P', 'Rome')
